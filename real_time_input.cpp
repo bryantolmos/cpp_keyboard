6a200842cd2da4b61f6ebb452753b48c7c306e17
bryantolmos <olmosbryant@outlook.com>
2023-05-28 14:17:26 -0700

    updated os check and real time input

--- real_time_input.cpp
+++ real_time_input.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
-
 using namespace std;
 
-#include "mac_linux.h"
+#ifdef _WIN32
+    #include <conio.h>
+#elif defined(__APPLE__) || defined(__linux__)
+    #include <termios.h>
+    #include <unistd.h>
+#else 
+    #error "Unknown operating system. Cannot compile :("
+#endif
 
 int main() {
-    /* checking what operating system user has */
+    cout << "\nREAL TIME INPUT: PRESS 0 TO END PROGRAM" << endl;
+
     #ifdef _WIN32
-        /* WINDOWS SPECIFIC CODE HERE
-        cout << "running on windows" << endl; */
-    #elif __linux__
-        /* LINUX SPECIFIC CODE HERE
-        cout << "running on linux" << endl; */
-        real_time_input();
-    #elif __APPLE__
-        #include <TargetConditionals.h>
-        #if TARGET_OS_MAC
-            /* MAC-OS SPECIFIC CODE HERE
-            cout << "running on macOS" << endl; */
-            real_time_input();
-        #endif
-    #else 
-        cout << "[ERROR 34404] : unknown operating system cant compile" << endl;
-        return 34404;
+        while (true) {
+            if (_kbhit()) { /* check if a key is pressed */
+                char c = _getch(); /* read the pressed key */
+                if (c == '0') {
+                    break;
+                } else {
+                    cout << c << endl;
+                }
+            }
+        }
+    #elif defined(__APPLE__) || defined (__linux__)
+        /* set terminal to non-canonical mode */
+        struct termios oldSettings, newSettings;
+        tcgetattr(STDIN_FILENO, &oldSettings);
+        newSettings = oldSettings;
+        newSettings.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+
+        while (true) {
+            char c;
+            if (read(STDIN_FILENO, &c, 1) == 1) { /* reading a single character */
+                if (c == '0') {
+                    break;
+                } else {
+                    cout << c << endl;;
+                }
+            }
+        }
+        /* restore terminal settings */
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
     #endif
 
     return 0;
-
 }
